ba9e6387d13afbeb876535f70d39949fa5245fc9
Nicolas <nicolas.fontaine.974@outlook.fr>
2015-02-27 15:16:33 +0100

    Ajout des plateformes des escaliers et du menu

---
+++ Assets/Menu/curseur.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class curseur : MonoBehaviour {
+	public Texture2D curs;
+	
+	public void Start() {
+		Screen.showCursor = false; 
+	}
+	
+	public void OnGUI() {
+		Vector3 positionSouris = Input.mousePosition;
+		Rect positionCurseur = new Rect(positionSouris.x, Screen.height - positionSouris.y, curs.width, curs.height);
+		GUI.Label(positionCurseur,curs);
+	}
+}
