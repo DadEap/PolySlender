64e4ad0e483a42083f50212dfc7c54baaf466911
shynn05 <bastien.meunier1@gmail.com>
2015-02-27 18:06:24 +0100

    Slender
    Ajout début script slender

---
+++ Assets/Scripts/SlenderDeplacement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlenderDeplacement : MonoBehaviour {
+
+	public GameObject player;
+	public float radius;
+	public bool inRange;
+	public bool inSight;
+	bool justwarp;
+	SphereCollider rangeCollider;
+	Vector3 sightRange;
+	NavMeshAgent agent;
+	public int timeWarping;
+	int compteur;
+	Vector3 target;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		inRange = false;
+		inSight = false;
+		radius = 30;
+		justwarp = false;
+		compteur = 0;
+		timeWarping = 100;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (inSight)
+		{
+			agent.SetDestination (player.transform.position);
+		} 
+		else if (inRange)
+		{
+
+		}
+		else
+		{
+			if(!justwarp)
+			{
+				target = Random.insideUnitSphere*radius;
+				target += transform.position;
+				NavMeshHit hit;
+				NavMesh.SamplePosition(target,out hit,radius,1);
+				agent.Warp(hit.position);
+				justwarp = true;
+
+			}
+			else
+			{
+				compteur++;
+			}
+		}
+		if (compteur > timeWarping)
+		{
+			justwarp = false;
+			compteur = 0;
+		}
+	
+	}
+}
