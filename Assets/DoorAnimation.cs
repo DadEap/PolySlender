93df9e3efacef56c45ffdbb013d0b8dd15d83e62
ARenaudeau <renaudeau.armand@gmail.com>
2015-03-08 10:42:53 +0100

    Added door animations.

---
+++ Assets/DoorAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorAnimation : MonoBehaviour {
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		animator.SetBool ("Open", false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		animator.SetBool ("Open", true);
+	}
+}
