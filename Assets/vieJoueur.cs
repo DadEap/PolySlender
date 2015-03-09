1ea1eb874726db1048594e3fad4be3e8bf2e169c
Nicolas <nicolas.fontaine.974@outlook.fr>
2015-03-09 13:11:36 +0100

    modification du script pour la vie du personnage

--- Assets/vieJoueur.cs
+++ Assets/vieJoueur.cs
@@ -12,25 +12,23 @@ public class vieJoueur : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float distance = GameObject.Find("slender").GetComponent<SlenderDeplacement>().distance;
-		if (distance == 0)
-			vie = vie - 100;
-		else if (distance < 10)
-			vie = vie - 30;
-		else if (distance < 20)
-			vie = vie - 20;
-		else if (vie < 30)
-			vie = vie - 10;
-		else 
-			vie += 10;
-
-		if (vie <= 0) 
-		{
-			Time.timeScale = 0f; // Le temps s'arrete
-			GUI.TextArea(new Rect (Screen.width / 2 - 40, Screen.height / 2 - 20, 100, 40), "Game Over");
-			if (GUI.Button (new Rect (Screen.width / 2 - 40, Screen.height / 2 - 30, 100, 40), "Menu Principal"))
-					Application.LoadLevel ("menu"); 
-		}
-
+		if (vie <= 0) {
+						Time.timeScale = 0f; // Le temps s'arrete
+						GUI.TextArea (new Rect (Screen.width / 2 - 40, Screen.height / 2 - 20, 100, 40), "Game Over");
+						if (GUI.Button (new Rect (Screen.width / 2 - 40, Screen.height / 2 - 30, 100, 40), "Menu Principal"))
+								Application.LoadLevel ("menu"); 
+				} else {
+						float distance = GameObject.Find ("slender").GetComponent<SlenderDeplacement> ().distance;
+						if (distance == 0)
+								vie = vie - 100;
+						else if (distance < 10)
+								vie = vie - 30;
+						else if (distance < 20)
+								vie = vie - 20;
+						else if (vie < 30)
+								vie = vie - 10;
+						else 
+								vie += 10;
+				}
 	}
 }
