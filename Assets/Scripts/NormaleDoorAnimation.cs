eae25cb40211c012f4ca7605fe2a6dad16e8ead1
ARenaudeau <renaudeau.armand@gmail.com>
2015-03-13 18:50:23 +0100

    Added normal door.

---
+++ Assets/Scripts/NormaleDoorAnimation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NormaleDoorAnimation : MonoBehaviour {
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		bool open = true;
+		anim.Play ("Armature|Open");
+	}
+
+	void OnTriggerExit(Collider collider)
+	{
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
