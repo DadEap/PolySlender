0969462be0353ce6ce80d5e7811b5bdd77ffa049
charlie-misscha <charlie.mathonnet@gmail.com>
2015-03-14 16:43:56 +0100

    GitHub c'est de la merde
    annulation de la modification involontaire et totalement aléatoire de ce
    fichier

--- Assets/Scripts/vieJoueur.cs
+++ Assets/Scripts/vieJoueur.cs
@@ -4,14 +4,14 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class vieJoueur : MonoBehaviour {
-
+	
 	#region Attributs
-
+	
 	public float vie; 
 	public double dist;
-	private bool isOver;
+	public GameObject gameOver; // l'objet (prefab) à instancier lors du Game Over
 	private GameObject slender;
-
+	
 	#endregion
 	
 	#region Proprietes
@@ -21,66 +21,56 @@ public class vieJoueur : MonoBehaviour {
 	#endregion
 	
 	#region Methodes
-
+	
 	// Use this for initialization
 	void Start () {
 		vie = 100.0f;
 		dist = Mathf.Infinity;
 		slender = GameObject.Find("slender");
-		isOver = false;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		//Debug.Log (vie);
 		if (vie <= 0) 
 		{
-			GameObject over = GameObject.Find("GameOver");
-			over.SetActive(true);
-			Time.timeScale = 0f; // Le temps s'arrete
-			isOver = true;
+			// déclenchement du Game Over :
+			Instantiate(gameOver, Vector3.zero, Quaternion.identity);
+			// la condition d'arret du jeu est atteinte, on se débarrasse donc de ce script
+			// cela évite de créer en boucle des GameOver
+			Destroy(this);
 		}
 		else
 		{
 			dist = ((SlenderDeplacement)slender.GetComponent ("SlenderDeplacement")).getDistance ();
 			if (dist < 0f) 
 			{
-				vie = vie - 100;
-
+				// comment on a une distance inférieure à 0 ?
+				vie = vie - 100*Time.deltaTime;
+				
 			} 
 			else if (dist < 5f) 
 			{
-				vie = vie - 0.1f;
+				vie = vie - 20f*Time.deltaTime;
 			} 
 			else  if (dist < 10f) 
 			{
-				vie = vie - 0.05f;
+				vie = vie - 5f*Time.deltaTime;
 			} 
 			else if (vie < 15f) 
 			{
-				vie = vie - 0.01f;
+				vie = vie - 1f*Time.deltaTime;
 			} 
 			else 
 			{
 				if (vie < 100)
-					vie += 0.5f;
+					vie += 5f*Time.deltaTime;
 				if (vie >= 100)
 					vie = 100;
 			}
 		}
 	}
-	/*
-	void OnGui ()
-	{
-		if (isOver) 
-		{
-			//GUI.TextArea (new Rect (Screen.width / 2 - 40, Screen.height / 2 - 20, 100, 40), "Game Over");
-			if (GUI.Button (new Rect (Screen.width / 2 - 40, Screen.height / 2 - 30, 100, 40), "Menu Principal"))
-			{
-				Application.LoadLevel ("menu");
-			}
-
-		}
-	}*/
+	
 	#endregion
 }
