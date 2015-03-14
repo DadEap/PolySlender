7848fc2de95f540f53d4014bd0f1c38826d61730
DadEap <boas.dav@gmail.com>
2015-03-14 20:16:31 +0100

    Ajustement des piliers, et gestion de l'endurance OK

--- Assets/Scripts/FootStepsSound.cs
+++ Assets/Scripts/FootStepsSound.cs
@@ -3,22 +3,28 @@ using System.Collections;
 
 public class FootStepsSound : MonoBehaviour {
 	
-	public AudioClip[] concreteSoundStep;
-	public AudioClip[] woodSoundStep;
-	public AudioClip[] metalSoundStep;
-	public AudioClip[] terrainSoundStep;
-	public AudioClip breathless;
+	public AudioClip concreteSoundStep;
+	//public AudioClip[] woodSoundStep;
+	//public AudioClip[] metalSoundStep;
+	//public AudioClip[] terrainSoundStep;
+	//public AudioClip breathless;
 	private CharacterController controller;
 	private CharacterMotor motor;
+
 	public float normalSpeed;
 	public float sprintSpeed;
 	public float delayBetweenStep;
-	public float delayBetweenBreathless;
+	//public float delayBetweenBreathless;
+	public float delayBetweenRun;
+
 	public int maxEndurance;
 	public int perteEndurance;
 	public int gainEndurance;
+
 	private float nextPlay;
-	private float nextPlayBreathless;
+	private float nextRun;
+
+	//private float nextPlayBreathless;
 	private float delay;
 	private AudioClip step;
 	private int endurance;
@@ -31,7 +37,7 @@ public class FootStepsSound : MonoBehaviour {
 		motor = GetComponent<CharacterMotor>();
 		controller = GetComponent<CharacterController>();
 		delay = delayBetweenStep;
-		endurance = 900;
+		endurance = maxEndurance;
 	}
 	
 	
@@ -48,7 +54,7 @@ public class FootStepsSound : MonoBehaviour {
 				switch (hit.collider.tag)
 				{
 				case "concrete":*/
-					step = concreteSoundStep[0];
+					step = concreteSoundStep;
 					/*break;
 				case "wood":
 					step = woodSoundStep[Random.Range(0, 4)];
@@ -64,21 +70,27 @@ public class FootStepsSound : MonoBehaviour {
 				audio.Play();
 			}
 		}
-
+		/*
 		if ( (endurance == 0) & (Time.time > nextPlayBreathless) ) {
 			nextPlayBreathless = Time.time + delayBetweenBreathless;
 			audio.PlayOneShot (breathless);
 		}
-		
-		if (Input.GetKey(KeyCode.LeftShift))
+		*/
+
+		if (Input.GetKey(KeyCode.LeftShift) && endurance > 0 && Time.time > nextRun )
 		{
-			//nextPlayBreathless = Time.time + delayBetweenBreathless;
-			motor.movement.maxForwardSpeed = sprintSpeed;
-			motor.movement.maxSidewaysSpeed = sprintSpeed;
-			motor.movement.maxBackwardsSpeed = sprintSpeed;
-			delayBetweenStep = delay/2;
-			//endurance = endurance - perteEndurance;
-			return;
+			if (Input.GetKey (KeyCode.Z) || Input.GetKey (KeyCode.Q) || Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.D)) {
+				//nextPlayBreathless = Time.time + delayBetweenBreathless;
+				motor.movement.maxForwardSpeed = sprintSpeed;
+				motor.movement.maxSidewaysSpeed = sprintSpeed;
+				motor.movement.maxBackwardsSpeed = sprintSpeed;
+				delayBetweenStep = delay / 2;
+				endurance = endurance - perteEndurance;
+				if (endurance <= 0) {
+					nextRun = Time.time + delayBetweenRun;
+				}
+				return;
+			}
 		}
 		else 
 		{
@@ -86,11 +98,11 @@ public class FootStepsSound : MonoBehaviour {
 			motor.movement.maxSidewaysSpeed = normalSpeed;
 			motor.movement.maxBackwardsSpeed = normalSpeed;
 			delayBetweenStep = delay;
-			/*
+
 			if (endurance < maxEndurance) {
 				endurance = endurance + gainEndurance;
 			}
-			*/
+
 			return;
 		}
 
