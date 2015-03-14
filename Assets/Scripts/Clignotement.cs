0b9ebcd934aafd68fcfc99f5e6f4c1b76d0331b7
charlie-misscha <charlie.mathonnet@gmail.com>
2015-03-14 17:21:46 +0100

    Brouillage de l'écran
    lorsque l'on perd de la vie, l'écran se brouille et un son désagréable
    apparaît
    l'objet qui fait ça est attaché  à la caméra du FPController
    oui, j'avoue, j'ai entièrement changé le script, je ne comprends pas les
    IEnumerators

--- Assets/Scripts/Clignotement.cs
+++ Assets/Scripts/Clignotement.cs
@@ -3,20 +3,23 @@ using System.Collections;
 
 public class Clignotement : MonoBehaviour {
 
-	public int frequence;
-	private float timer;
+	private vieJoueur scriptVieJoueur;
+	private AudioSource son;
 
 	// Use this for initialization
-	IEnumerator Start () {
-		while (true) {
-			timer = frequence * Random.value*Random.value;
-			yield return new WaitForSeconds(timer);
-			light.enabled = !light.enabled;
-		}
+	void Start () {
+		scriptVieJoueur = (vieJoueur) GameObject.FindGameObjectWithTag("Player").GetComponent("vieJoueur");
+		son = (AudioSource) GetComponent ("AudioSource");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void FixedUpdate () {
+		int offsetTexture = Random.Range(1,20);
+		float frequenceTexture = (100-scriptVieJoueur.vie)/100;
+		int couleurTexture = Random.Range (1, 5);
+		// borné entre 0 et 1 pour etre sur que ça pète pas les oreilles
+		float volumeSon = Mathf.Min(1, Mathf.Max(0,(50 - scriptVieJoueur.vie + Random.Range(0,25))/200));
+		renderer.material.SetTextureOffset ("_MainTex", new Vector2 (offsetTexture, offsetTexture));
+		renderer.material.color = new Color(couleurTexture,couleurTexture,couleurTexture,Random.Range(0f,frequenceTexture));
+		son.volume = volumeSon;
 	}
 }
